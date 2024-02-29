f1e26cf70a537a3b27dc9fca48a175e123bcf364
Marc <wanner@chalmers.se>
2024-02-29 10:27:53 +0100

    skeleton for best arm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from constraint_explore import BernoulliBandit
 
 def f(x):
     return (np.sin(13*x)*np.sin(27*x) + 1)/2
@@ -30,17 +31,16 @@ def get_lip(L:float):
     return lambda dx: L*dx
 
 def main():
-    
-    x = np.linspace(0, 1, 1000)
-    #plt.plot(x, np.mean(randf(np.stack([x for _ in range(100000)])), axis=0))
-    #plt.show()
-    #return
-    fig, ax = plt.subplots()
-    #ax.plot(x, DOO_example1(x), c="r", linewidth=1)
+    n_arms = 10
+    x = np.linspace(0, 1, n_arms)
+    exp_arms = f(x)
+    bandit = BernoulliBandit(expected_rewards=exp_arms)
 
 
+    best_arm = 1
 
-
+    plt.plot(x, f(x))
+    plt.axvline(x[best_arm])
     plt.show()
 
 
